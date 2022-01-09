d0632dc61ec82446cbf9cef4b51b24352fe18318
ski <ski@skis-MacBook-Pro.local>
2022-01-09 19:16:56 +0300

    finished front-end. having a pb with the polygon mumbai network. We send update if not working by tomorrow

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const { ethers } = require("hardhat");
+
+
+
+const main = async () => {
+
+
+
+  const Market = await ethers.getContractFactory('NFTMarket')
+  const market = await Market.deploy()
+  await market.deployed()
+  const marketAddress = market.address
+
+  console.log("NftMarket deployed to: ", marketAddress)
+
+  const NFT = await ethers.getContractFactory("NFT")
+  const nft = await NFT.deploy(marketAddress)
+  await nft.deployed()
+  const nftContractAddress = nft.address
+  console.log("Nft deployed to: ", nftContractAddress)
+
+  
+
+}
+
+main()
